d724408dd9c0989910766340872fa4639c340e4f
Elena Tolmacheva <urfi-urfi@yandex.ru>
2023-07-26 11:44:49 +0600

    add chat project v1

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Chat
+
+This project is a simple chat application that allows users to connect to a chat server and exchange messages with other users in real-time. The chat client has a graphical user interface (GUI) built using Java Swing.
+
+## Features
+
+- Connect to a chat server by providing the server address and port number.
+- Choose a unique username to be identified in the chat room.
+- View the list of online users.
+- Send and receive messages in the chat room.
+- Get notifications about user connections and disconnections.
+
+## Technologies Used
+
+- Java
+- Java Swing for GUI
+- Sockets for network communication
+
+## Getting Started
+
+1. Clone the repository to your local machine:
+````
+ git clone https://github.com/your_username/chat-client.git
+````
+
+2. Start the chat server by entering this command in the repository directory:
+
+
+````
+java chat.Server
+````
+
+3. Enter the correct port number, for example, 5555.
+
+4. Open a new tab in the terminal and start the chat client:
+
+````
+java chat.client.ClientGuiController
+````
+
+5. If you are running the project on a single computer, enter "localhost" in the open tab. If you are running the project on multiple computers, enter your IP address and the port number (e.g., 172.17.255.255:5555).
+
+6. You can also start a bot that writes the time by using the following command:
+
+````
+java chat.client.BotClient
+````
+and repeat step 5.
+## Autors
+[@Tyomnayark] (https://github.com/Tyomnayark)
---
+++ chat/client/ClientGuiView.java
@@ -0,0 +1,132 @@
+package chat.client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientGuiView {
+    private final ClientGuiController controller;
+
+    private JFrame frame = new JFrame("Чат");
+    private JTextField textField = new JTextField(50);
+    private JTextArea messages = new JTextArea(10, 50);
+    private JTextArea users = new JTextArea(10, 10);
+    Font newFont = new Font("Arial", Font.BOLD, 20);
+    Color darkGray = new Color(64, 64, 64);
+    Color white = Color.WHITE;
+
+    public ClientGuiView(ClientGuiController controller) {
+        this.controller = controller;
+        initView();
+    }
+
+    private void initView() {
+        textField.setEditable(false);
+        textField.setFont(newFont);
+        textField.setForeground(white);
+        textField.setBackground(darkGray);
+
+        messages.setEditable(false);
+        messages.setFont(newFont);
+        messages.setForeground(white);
+        messages.setBackground(darkGray);
+        messages.setLineWrap(true);
+        messages.setWrapStyleWord(true);
+
+        users.setEditable(false);
+        users.setFont(newFont);
+        users.setForeground(white);
+        users.setBackground(darkGray);
+        users.setLineWrap(true);
+        users.setWrapStyleWord(true);
+
+        frame.getContentPane().setBackground(darkGray);
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(textField, BorderLayout.NORTH);
+        frame.getContentPane().add(new JScrollPane(messages), BorderLayout.WEST);
+        frame.getContentPane().add(new JScrollPane(users), BorderLayout.EAST);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        textField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                controller.sendTextMessage(textField.getText());
+                textField.setText("");
+            }
+        });
+    }
+
+
+    public String getServerAddress() {
+        return JOptionPane.showInputDialog(
+                frame,
+                "Введите адрес сервера:",
+                "Конфигурация клиента",
+                JOptionPane.QUESTION_MESSAGE);
+    }
+
+    public int getServerPort() {
+        while (true) {
+            String port = JOptionPane.showInputDialog(
+                    frame,
+                    "Введите порт сервера:",
+                    "Конфигурация клиента",
+                    JOptionPane.QUESTION_MESSAGE);
+            try {
+                return Integer.parseInt(port.trim());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(
+                        frame,
+                        "Был введен некорректный порт сервера. Попробуйте еще раз.",
+                        "Конфигурация клиента",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public String getUserName() {
+        return JOptionPane.showInputDialog(
+                frame,
+                "Введите ваше имя:",
+                "Конфигурация клиента",
+                JOptionPane.QUESTION_MESSAGE);
+    }
+
+    public void notifyConnectionStatusChanged(boolean clientConnected) {
+        textField.setEditable(clientConnected);
+        if (clientConnected) {
+            JOptionPane.showMessageDialog(
+                    frame,
+                    "Соединение с сервером установлено",
+                    "Чат",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(
+                    frame,
+                    "Клиент не подключен к серверу",
+                    "Чат",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
+    public void refreshMessages() {
+        messages.append(controller.getModel().getNewMessage() + "\n");
+    }
+
+    public void refreshUsers() {
+        ClientGuiModel model = controller.getModel();
+        StringBuilder sb = new StringBuilder();
+        for (String userName : model.getAllUserNames()) {
+            sb.append(userName).append("\n");
+        }
+        users.setText(sb.toString());
+    }
+}
